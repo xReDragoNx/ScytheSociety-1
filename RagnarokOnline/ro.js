b27400d853e02464d5f3a4e28513a4da799d3b55
xReDragoNx <xredragonx@gmail.com>
2024-12-12 13:30:00 +0100

    Json de Imagenes
    Cree un json que contenga la estructura de las imagenes de misiones

--- RagnarokOnline/ro.js
+++ RagnarokOnline/ro.js
@@ -42,3 +42,35 @@ function copyToClipboard(text) {
   }, 2000); // Vuelve al texto original después de 2 segundos
 }
 /*fin de navi copiab le*/
+
+// Ruta al archivo JSON que genera las imagenes de las misiones
+const jsonFile = "romisiones.json";
+
+// Contenedor donde se agregarán las misiones
+const container = document.getElementById("misiones-container");
+
+// Cargar el archivo JSON y generar el contenido
+fetch(jsonFile)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((mision) => {
+      // Crear el elemento HTML para cada misión
+      const misionElement = document.createElement("div");
+      misionElement.className = "col-lg-2 col-md-3 col-sm-4 col-12 mb-4";
+
+      misionElement.innerHTML = `
+        <a href="${mision.link}">
+          <div class="image-container">
+            <img src="${mision.imagen}" class="img-fluid" alt="${mision.descripcion}">
+          </div>
+        </a>
+        <h5 class="text-center">
+          <a href="${mision.link}">${mision.titulo}</a>
+        </h5>
+      `;
+
+      // Agregar el elemento al contenedor
+      container.appendChild(misionElement);
+    });
+  })
+  .catch((error) => console.error("Error cargando el archivo JSON:", error));
